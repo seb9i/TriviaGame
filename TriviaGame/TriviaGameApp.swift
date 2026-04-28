d6cee324c08fca897cf59715edeed94f64862028
BT_C09_13 <mobileappdev@bths.edu>
2026-04-28 08:50:05 -0400

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Student on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
