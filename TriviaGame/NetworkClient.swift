240de3db8bab51e928e49397fdac7fe03774c8d0
BT_C09_13 <mobileappdev@bths.edu>
2026-04-30 09:19:18 -0400

    Added functionality to modify category and difficulty

--- TriviaGame/NetworkClient.swift
+++ TriviaGame/NetworkClient.swift
@@ -10,10 +10,37 @@ import Foundation
 @Observable
 class NetworkClient{
     private(set) var triviaQuestions: [Trivia] = []
-    
+    let triviaCategories: [String: String] = [
+        "General Knowledge": "9",
+        "Entertainment: Books": "10",
+        "Entertainment: Film": "11",
+        "Entertainment: Music": "12",
+        "Entertainment: Musicals & Theatres": "13",
+        "Entertainment: Television": "14",
+        "Entertainment: Video Games": "15",
+        "Entertainment: Board Games": "16",
+        "Science & Nature": "17",
+        "Science: Computers": "18",
+        "Science: Mathematics": "19",
+        "Mythology": "20",
+        "Sports": "21",
+        "Geography": "22",
+        "History": "23",
+        "Politics": "24",
+        "Art": "25",
+        "Celebrities": "26",
+        "Animals": "27",
+        "Vehicles": "28",
+        "Entertainment: Comics": "29",
+        "Science: Gadgets": "30",
+        "Entertainment: Japanese Anime & Manga": "31",
+        "Entertainment: Cartoon & Animations": "32"
+    ]
+    private var difficulty: String?
+    private var category: String?
     private(set) var triviaResponse: Trivia?
     func getNowPlaying() async {
-        let urlString = "https://opentdb.com/api.php?amount=10&difficulty=easy"
+        let urlString = "https://opentdb.com/api.php?amount=10&difficulty=\(difficulty ?? "easy")&category=\(triviaCategories[category ?? "General Knowledge"] ?? "9")"
         let url : URL? = URL(string: urlString)
         guard let urlUnwrapped = url else {
             return
@@ -31,5 +58,11 @@ class NetworkClient{
         }
     }
     
+    func changeDifficulty(to difficultyTo: String) async {
+        difficulty = difficultyTo
+    }
+    func changeCategory(to categoryTo: String) async {
+        category = categoryTo
+    }
    
 }
